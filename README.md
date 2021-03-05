18c0dfb98fe3ca77b0fbb42ef7d3061f302b74ab
Smallo <smallo92@gmail.com>
2021-03-05 11:28:29 +1000

    v1.0.0
    Initial Push

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-PlantGrowProgress
+# Plant Grow Progress
+This mod shows a plant grow progress when you hover over it. It changes colour depending on the progress. Super simple, that's it.
+
+## Manual Installation
+To install this mod, you need to have BepInEx. After installing BepInEx, extract ShowPlantProgress.dll into games install **"\Valheim\BepInEx\plugins"**
+
+If you have any suggestions, feel free to let me know!
+
+## Changelog
+
+#### v1.0.0;
+* Initial release
+
+## Images
+
+![Show Off](https://fivem.fail/gta5/Network/NetworkIsTutorialSessionChangePending/tRaNsMKBm.png)
---
+++ ShowPlantProgress/ShowPlantProgress.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace ShowPlantProgress
+{
+    [BepInPlugin("smallo.mods.showplantprogress", "Show Plant Progress", "1.0.0")]
+    [HarmonyPatch]
+    class ShowPlantProgressPlugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Plant), "GetHoverText")]
+        public static string PlantGetHoverText_Patch(string __result, Plant __instance)
+        {
+            if (__instance == null) return __result;
+            int percentage = (int)Mathf.Floor((float)__instance.TimeSincePlanted() / (float)__instance.GetGrowTime() * 100);
+            string color = "red";
+            if (percentage >= 25 && percentage <= 49) color = "orange";
+            if (percentage >= 50 && percentage <= 74) color = "yellow";
+            if (percentage >= 75 && percentage <= 100) color = "lime";
+            string growPercentage = $"<color={color}>{percentage}%</color>";
+
+            return __result.Replace(" )", $", {growPercentage} )");
+        }
+    }
+}
