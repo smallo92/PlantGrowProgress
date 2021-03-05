009cb229f3a5867f8e386e5ab8c66a4a68afde22
Smallo <smallo92@gmail.com>
2021-03-06 09:20:36 +1000

    v1.2.1
    - Added support for berry bushes

--- README.md
+++ README.md
@@ -8,9 +8,16 @@ If you have any suggestions, feel free to let me know!
 
 ## Changelog
 
+#### v1.2.1;
+* Readme update
+
+#### v1.2.0;
+* Added support for berry bushes
+
 #### v1.0.0;
 * Initial release
 
 ## Images
 
 ![Show Off](https://fivem.fail/gta5/Network/NetworkIsTutorialSessionChangePending/tRaNsMKBm.png)
+![Raspberry Bush](https://fivem.fail/gta5/Audio/FindRadioStationIndex/NCG4081BEb.png)
--- ShowPlantProgress/ShowPlantProgress.cs
+++ ShowPlantProgress/ShowPlantProgress.cs
@@ -1,18 +1,19 @@
 ﻿using BepInEx;
 using HarmonyLib;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
 namespace ShowPlantProgress
 {
-    [BepInPlugin("smallo.mods.showplantprogress", "Show Plant Progress", "1.0.0")]
+    [BepInPlugin("smallo.mods.showplantprogress", "Show Plant Progress", "1.2.1")]
     [HarmonyPatch]
     class ShowPlantProgressPlugin : BaseUnityPlugin
     {
-        void Awake()
-        {
-            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
-        }
+        private static readonly List<string> bushList = new List<string> { "RaspberryBush(Clone)", "BlueberryBush(Clone)", "CloudberryBush(Clone)" };
+
+        void Awake() => Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(Plant), "GetHoverText")]
@@ -20,13 +21,43 @@ namespace ShowPlantProgress
         {
             if (__instance == null) return __result;
             int percentage = (int)Mathf.Floor((float)__instance.TimeSincePlanted() / (float)__instance.GetGrowTime() * 100);
-            string color = "red";
-            if (percentage >= 25 && percentage <= 49) color = "orange";
-            if (percentage >= 50 && percentage <= 74) color = "yellow";
-            if (percentage >= 75 && percentage <= 100) color = "lime";
-            string growPercentage = $"<color={color}>{percentage}%</color>";
+            string colour = GetColour(percentage);
+            string growPercentage = $"<color={colour}>{percentage}%</color>";
 
             return __result.Replace(" )", $", {growPercentage} )");
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Pickable), "GetHoverText")]
+        public static string GameObjectHoverText_Patch(string __result, Pickable __instance)
+        {
+            if (!bushList.Contains(__instance.name)) return __result;
+
+            DateTime startTime = new DateTime(__instance.m_nview.GetZDO().GetLong("picked_time"));
+            double percentage = (ZNet.instance.GetTime() - startTime).TotalMinutes / __instance.m_respawnTimeMinutes * 100;
+            if (percentage > 99.99f) return __result;
+
+            string colour = GetColour(Math.Round(percentage, 2));
+            string growPercentage = $"<color={colour}>{percentage:0.00}%</color>";
+
+            return __result + $"{__instance.name.Replace("(Clone)", "").Replace("Bush", " Bush")} ( {growPercentage} )";
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Plant), "Awake")]
+        public static void PlantBiome_Patch(Plant __instance)
+        {
+            __instance.m_biome = Heightmap.Biome.BiomesMax;
+        }
+
+        public static string GetColour(double percentage)
+        {
+            string colour = "red";
+            if (percentage >= 25 && percentage <= 49) colour = "orange";
+            if (percentage >= 50 && percentage <= 74) colour = "yellow";
+            if (percentage >= 75 && percentage <= 100) colour = "lime";
+
+            return colour;
         }
     }
 }
